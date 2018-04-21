bf7fc79fe13fbe2f10b6d736f764628b57f11949
Floris Meininger <florismeininger@gmail.com>
2018-04-21 15:11:07 +0200

    create patient endpoint with simple validation

--- backend/initialize.php
+++ backend/initialize.php
@@ -1,12 +1,5 @@
 <?php
 
-  define("PRIVATE_PATH", dirname(__FILE__));
-  define("PROJECT_PATH", dirname(PRIVATE_PATH));
-  define("PUBLIC_PATH", PROJECT_PATH . '/public');
-  define("SHARED_PATH", PRIVATE_PATH . '/shared');
-
-  session_start();
-
   require_once('functions.php');
   require_once('database.php');
   require_once('query_functions.php');
