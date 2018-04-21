f60fc82eeb66b9957794a30339d30eaed2902657
Rik Aikman <rikaikman@hotmail.com>
2018-04-21 12:56:45 +0200

    prepared statement

--- backend/index.php
+++ backend/index.php
@@ -2,10 +2,17 @@
 //============================== PREVENTIE SQL INJECTION ==============================
 
     //------------ PREPARED STATEMENTS VIA PDO CLASS ------------
-        $db = UMCregistratie;
+        $db = "UMCregistratie";
+        $host = "";
+        $username = "root";
+        $pw = "";
+
+        $conn = mysqli_connect($sn, $us, $pw, $db);
 
         $preparedStatement = $db->prepare('INSERT INTO table (column) VALUES (:column)');
 
         $preparedStatement->execute(array('column' => $unsafeValue));
+            
+        $result = $conn->query($sql)
     
         
