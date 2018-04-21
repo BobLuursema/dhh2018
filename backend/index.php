f250fdb2ebd93c016712ac7b79ec0786db8d4135
Rik Aikman <rikaikman@hotmail.com>
2018-04-21 12:56:33 +0200

    prepared statement

--- backend/index.php
+++ backend/index.php
@@ -1,3 +1,11 @@
 <?php
+//============================== PREVENTIE SQL INJECTION ==============================
 
+    //------------ PREPARED STATEMENTS VIA PDO CLASS ------------
+        $db = UMCregistratie;
+
+        $preparedStatement = $db->prepare('INSERT INTO table (column) VALUES (:column)');
+
+        $preparedStatement->execute(array('column' => $unsafeValue));
     
+        
