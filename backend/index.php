06f9451b74f8dbc5e3197a07d8820d807c2f667e
Rik Aikman <rikaikman@hotmail.com>
2018-04-21 13:55:12 +0200

    prepared sql getyped

--- backend/index.php
+++ backend/index.php
@@ -2,17 +2,12 @@
 //============================== PREVENTIE SQL INJECTION ==============================
 
     //------------ PREPARED STATEMENTS VIA PDO CLASS ------------
-        $db = "UMCregistratie";
-        $host = "";
-        $username = "root";
-        $pw = "";
+        
+        global $db;
 
         $conn = mysqli_connect($sn, $us, $pw, $db);
-
-        $preparedStatement = $db->prepare('INSERT INTO table (column) VALUES (:column)');
-
-        $preparedStatement->execute(array('column' => $unsafeValue));
-            
-        $result = $conn->query($sql)
     
         
+    $preparedStatement = $db->prepare('INSERT INTO patienten(voorletters, roepnaam, voorvoegsel, geboortenaam, geboortedatum, geslacht, meerling, partnernaam, voorvoegsel_partnernaam, telefoon, mobiel, email, woonplaats, postcode, huisnummer, toevoeging, land, foto, patientnummer, correspondentie_adres, huisarts, tandarts, apotheek, toestemming_ophalen_gegevens, akkoord_privacy_policy) VALUES (:voorletters,:roepnaam,:voorvoegsel,:geboortenaam,'.$geboortedatum.',:geslacht,:meerling,:partnernaam,:voorvoegsel_partnernaam,:telefoon,:mobiel,:email,:woonplaats,:postcode,'.$huisnummer.',:toevoeging,:land,:foto,'.patientnummer.',:correspondentie_adres,:huisarts,:tandarts,:apotheek,'.$toestemming_ophalen_gegevens.','.$akkoord_privacy_policy.')');
+  
+    $preparedStatement->execute(array('voorletters' => $voorletters, 'roepnaam' => $roepnaam, 'voorvoegsel'=> $voorvoegsel, 'geboortenaam'=>$geboortenaam,'geboortedatum'=>$geboortedatum,'geslacht'=> $geslacht,'meerling'=> $meerling, 'partnernaam'=> $partnernaam,'voorvoegsel_partnernaam'=>$voorvoegsel_partnernaam,'telefoon'=>$telefoon,'mobiel'=>$mobiel,'email'=>$email,'woonplaats'=>$woonplaats,'postcode'=>$postcode,'toevoeging'=>$toevoeging,'land'=>$land,'foto'=>$foto,'correspondentie_adres'=>$correspondentie_adres,'huisarts'=>$huisarts,'tandarts'=>$tandarts,'apotheek'=>$apotheek));
